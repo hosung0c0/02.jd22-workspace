b51a5cd5d056b2089cdef92b0a6de508ec9f577d
hosung0c0 <didghtjd1324@naver.com>
2022-10-25 16:18:44 +0900

    ss

--- 01.JAVA-Root/JavaDefault/src/MyJava.java
+++ 01.JAVA-Root/JavaDefault/src/MyJava.java
@@ -2,5 +2,7 @@ public class MyJava{
     public static void main(String[] args) {
         System.out.println("나야나");
         System.out.println("너두냐");
+        System.out.println("나두야");
+        System.out.println("너두냐");
     }   
 }
